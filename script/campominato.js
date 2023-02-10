de6ce5e3c1fda22f95e6a4a6b03be04e976a4081
Blaser993 <corradodepinto.design@gmail.com>
2023-02-10 15:51:20 +0100

    PUSH PRE TICKET

--- script/campominato.js
+++ script/campominato.js
@@ -31,11 +31,24 @@ function() {
         creoCella.append(i)
     }
 
-
-
-
-
 })
 
+// Generiamo le bombe
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe, facendo attenzione che:
+// nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// Quando l’utente clicca su una cella: se il numero della cella è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 
+
+const bombe = []
+
+while (bombe.lenght < LATOGRIGLIA){
+    let casellaBomba = (Math.random(1) * numeroCelle)
+    console.log(casellaBomba, bombe.length, bombe, "numero della bomba")
+    bombe.push(casellaBomba)
+}
+
+console.log(bombe, "numeri delle bombe")
